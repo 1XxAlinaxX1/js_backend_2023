121311827bfc119805cb4de9ea160b9865044402
1XxAlinaxX1 <ivanova-dz@mail.ru>
2023-06-24 01:08:32 +0300

    vse

--- server.js
+++ server.js
@@ -5,42 +5,43 @@ const {MongoClient, ObjectId} = require('mongodb');
 const app = express();
 const port = 8000;
 
-app.use(express.json());
+app.use(express.json())
 app.use(express.static('public'));
 
-async function getDbCollection(dbAddress, dbName, dbCollectionName) {
+async function getDbCollectoin(dbAddress, dbName, dbCollectionName){
 	const client = new MongoClient(dbAddress);
 	await client.connect();
 	const db = client.db(dbName);
 	return db.collection(dbCollectionName);
-}	
-app.get('/tasks',async function(req, res){
-	const collection = await getDbCollection('mongodb://127.0.0.1','todoapp','tasks');
+}
+
+app.get('/zakaz', async function(req, res){
+	const collection = await getDbCollectoin('mongodb://127.0.0.1','todo', 'zakaz');
 	const data = await collection.find({}).toArray();
 	res.send(data);
 });
 
-app.get('/tasks/:id', async function(req, res){
-	const collection = await getDbCollection('mongodb://127.0.0.1','todoapp','tasks');
+app.get('/zakaz/:id', async function(req, res){
+	const collection = await getDbCollectoin('mongodb://127.0.0.1','todo', 'zakaz');
 	const data = await collection.findOne({_id: new ObjectId(req.params.id)});
 	res.send(data);
 });
 
-app.post('/tasks', async function(req, res){
-	const task = {...req.body, done: false};
-	const collection = await getDbCollection('mongodb://127.0.0.1','todoapp','tasks');
+app.post('/zakaz', async function(req, res){
+	const task={...req.body, done: false}
+	const collection = await getDbCollectoin('mongodb://127.0.0.1','todo', 'zakaz');
 	await collection.insertOne(task);
 	res.send(task);
 });
 
-app.patch('/tasks/:id', async function(req, res){
-	const collection = await getDbCollection('mongodb://127.0.0.1','todoapp','tasks');
+app.patch('/zakaz/:id', async function(req, res){
+	const collection = await getDbCollectoin('mongodb://127.0.0.1','todo', 'zakaz');
 	const data = await collection.updateOne({_id: new ObjectId(req.params.id)}, {'$set': req.body});
 	res.send({});
 });
 
-app.delete('/tasks/:id', async function(req, res){
-	const collection = await getDbCollection('mongodb://127.0.0.1','todoapp','tasks');
+app.delete('/zakaz/:id', async function(req, res){
+	const collection = await getDbCollectoin('mongodb://127.0.0.1','todo', 'zakaz');
 	await collection.deleteOne({_id: new ObjectId(req.params.id)});
 	res.send({});
 });
